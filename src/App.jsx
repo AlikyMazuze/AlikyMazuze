f222bf0a5dc4b1b0bb048e0b248597cb714d0e1f
AlikyMazuze <106402062+AlikyMazuze@users.noreply.github.com>
2023-10-18 14:10:25 +0200

    Uploadingfiles

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+    <header></header>
+    <main>
+      <h1>Hello World</h1>
+    </main>
+    <footer></footer>
+    </>
+  )
+}
+
+export default App
